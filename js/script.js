b8cf0ef08e4d90223b45a700e3687b11a2ab8a75
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2024-01-24 16:02:50 -0800

    UI design of a portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =================== typing animation ========================== */
+var typed = new Typed(".typing", {
+    strings:["Product Designer", "Web Developer", "Graphics Designer", "YouTuber", " "],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
